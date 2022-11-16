2108c49cac96d5533c9ea150e03cd79eb787dc2b
npargas24 <114077181+npargas24@users.noreply.github.com>
2022-11-16 10:46:49 -0500

    Add files via upload

---
+++ gam_end_konane.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+}
+
+
+void whosTurn {
+
+}
+
+
+
+
+
+
+
+    void gameOver {
+    white_player_move = whateverClassTheyDid (perameters)
+    black_player_move = whateverClassTheyDid (perameters)
+
+if (white_player_turn == True ) {
+     if (white_player_move == 0) {
+        cout << "The game is over\n"
+        cout << "The winner is the white player\n"
+    }
+
+     if (black_player_move == 0) {
+        cout << "The game is over\n"
+        cout << "The winner is the black player\n"
+    }
+
+    else
+      cout <<  endl;
+    }
+   
+
+
+else {
+      if (white_player_move == 0) {
+        cout << "The game is over\n"
+        cout << "The winner is the white player\n"
+    }
+
+     if (black_player_move == 0) {
+        cout << "The game is over\n"
+        cout << "The winner is the black player\n"
+    }
+
+    else
+        cout << endl;
+    }
+    }
+
+
+
+
+
+/*   // used to determine if the game is ended, makes sure at least 1 player has a move to make
+bool isGameOver(char board[8][8]){
+    return getBlackLegalMoves(board).empty() && getWhiteLegalMoves(board).empty();
+} */
+
